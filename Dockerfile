fd0a32308c91f059821dce161454d7fe39c79fa4
InBaeJang <inbaejj117@gmail.com>
2020-09-24 10:07:34 +0900

    Add Login & Subscription

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:12
+
+# Setting working directory. All the path will be relative to WORKDIR
+WORKDIR /usr/src/app
+
+# Installing dependencies
+COPY package*.json ./
+RUN npm install
+RUN npm audit fix
+
+# Copying source files
+COPY . .
+
+## docker 데몬에 매핑
+EXPOSE 4000
+
+# Running the app
+CMD [ "npm", "run" ,"start" ]
+
+## Docker run command
+# docker run -d -p 4000:4000 --network=host apollo-prisma:latest
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+'use strict';
+
 // const { GraphQLServer } = require('graphql-yoga')
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server')
 const { PrismaClient } = require('@prisma/client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
+
+const { PubSub } = require('graphql-yoga');
+const pubsub = new PubSub()
 
 // Prisma client
 const prisma = new PrismaClient()
@@ -15,15 +26,39 @@ const typeDefs = gql`
     }
     type Mutation {
         post(url: String!, description: String!): Link!
+        signup(email: String!, password: String!, name: String!): AuthPayload
+        login(email: String!, password: String!): AuthPayload
+        vote(linkId: ID!): Vote
         ## Update a link    #practice
         # updateLink(id: ID!, url: String, description: String): Link
         ## Delete a link    #practice
-        # deleteLink(id: ID!): Link 
+        # deleteLink(id: ID!): Link
     }
     type Link {
         id: ID!
         description: String!
         url: String!
+        postedBy: User
+        votes: [Vote!]!
+    }
+    type AuthPayload{
+        token: String
+        user: User
+    }
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        links: [Link!]!
+    }
+    type Subscription {
+        newLink: Link
+        newVote: Vote
+    }
+    type Vote {
+        id: ID!
+        link: Link!
+        user: User!
     }
 `
 
@@ -35,43 +70,55 @@ const typeDefs = gql`
 // let idCount = links.length
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: async (parent, args, context) => {
-            return context.prisma.link.findMany()
-        }
-    },
-    Mutation: {
-        post: (parent, args, context, info) => {
-            const newLink = context.prisma.link.create({
-                data: {
-                    url: args.url,
-                    description: args.description,
-                },
-            })
-            return newLink
-        },
-        // 2
-        // post: (parent, args) => {
-        //     const link = {
-        //         id: `link-${idCount++}`,
-        //         description: args.description,
-        //         url: args.url,
-        //     }
-        //     links.push(link)
-        //     return link
-        // }
-    },
+    Query,
+    Mutation,
+    Subscription,
+    User,
+    Link,
+    Vote
+    // Query: {
+    //     info: () => `This is the API of a Hackernews Clone`,
+    //     feed: async (parent, args, context) => {
+    //         return context.prisma.link.findMany()
+    //     }
+    // },
+    // Mutation: {
+    //     post: (parent, args, context, info) => {
+    //         const newLink = context.prisma.link.create({
+    //             data: {
+    //                 url: args.url,
+    //                 description: args.description,
+    //             },
+    //         })
+    //         return newLink
+    //     },
+    // 2
+    // post: (parent, args) => {
+    //     const link = {
+    //         id: `link-${idCount++}`,
+    //         description: args.description,
+    //         url: args.url,
+    //     }
+    //     links.push(link)
+    //     return link
+    // }
+    // },
+}
+
+const context = req => {
+    return {
+        ...req,
+        prisma,
+        pubsub
+    }
 }
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: {
-        prisma,
-    }
+    context
 })
-server.listen().then(({ url }) => {
+server.listen({ port: 4000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
 
