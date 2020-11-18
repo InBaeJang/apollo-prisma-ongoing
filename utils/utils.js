c0860c0f2347ce26ea337a8b728684935f81d7ed
InBaeJang <inbaejj117@gmail.com>
2020-11-19 06:36:47 +0900

    except node_modules

--- utils/utils.js
+++ utils/utils.js
@@ -1,18 +1,18 @@
-const jwt = require('jsonwebtoken')
+import { verify } from 'jsonwebtoken'
 const APP_SECRET = 'GraphQL-is-aw3some'
 
 function getUserId(context) {
     const Authorization = context.req.get('Authorization')
     if (Authorization) {
         const token = Authorization.replace('Bearer ', '') // remove Bearer
-        const { userId } = jwt.verify(token, APP_SECRET)
+        const { userId } = verify(token, APP_SECRET)
         return userId
     }
 
     throw new Error('Not authenticated')
 }
 
-module.exports = {
+export default {
     APP_SECRET,
     getUserId,
 }
